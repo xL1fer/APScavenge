0bab5ea5af02ffc1eb82f9a7a18426136cc01ff0
xL1fer <luis.m.g.batista@gmail.com>
2024-03-28 01:14:56 +0000

    changed gunicorn to initialize 4 workers capable of handling up to 4 threads each

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,4 @@ python3 manage.py makemigrations --no-input
 python3 manage.py migrate --no-input
 python3 manage.py collectstatic --no-input
 
-gunicorn approject.wsgi:application --bind 0.0.0.0:8000
+gunicorn approject.wsgi:application --bind 0.0.0.0:8000 --workers=4 --threads=4
